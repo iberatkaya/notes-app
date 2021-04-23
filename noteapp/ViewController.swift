f8bcccb6a10592c06689100193355a4d46b12842
iberatkaya <beratkaya1998@gmail.com>
2021-04-23 16:20:50 +0300

    Initial Commit

---
+++ noteapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noteapp
+//
+//  Created by Ibrahim Berat Kaya on 23.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
